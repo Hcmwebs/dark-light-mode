bb6f812e7f874fdd263a0ba8fd64616ca77ce9b7
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-03 02:34:58 +1200

    updated the dates and users on the single posts
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -1,17 +1,16 @@
 import React from 'react'
+import moment from 'moment'
 
-const SinglePost = ({ title, length, body, userId }) => {
+const SinglePost = ({ title, length, body, userId, date }) => {
 	return (
 		<>
 			<article className='post'>
-				<h3>{title}</h3>
+				<h3>{title.slice(0, 20)}</h3>
 				<div className='post-info'>
-					<span>date</span>
-					<span>{length} min read</span>
-          <span>userId</span>
+					<span>{moment(date).format('dddd Do YYYY')}</span>
+					<span>{length} min read</span> <span>Written by {userId}</span>
 				</div>
 				<p>{body}</p>
-
 			</article>
 		</>
 	)
