7270350ae93ae6c3bcf83be4c144e42fe8f200c0
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-02 22:36:46 +1200

    tried to fix the toggle buttons

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-const Button = ({ MdLightMode, MdDarkMode, toggleTheme,active }) => {
-
+const Button = ({ MdLightMode, MdDarkMode, toggleTheme, active}) => {
 	return (
 		<>
 			<div
-				className={`${active ? 'toggle dark-mode' : 'toggle light-mode'}`}
+				className={`${active ? 'toggle light' : 'toggle dark'}`}
 				onClick={toggleTheme}>
 				<MdDarkMode className='moon' />
 				<div className='marker'></div>
