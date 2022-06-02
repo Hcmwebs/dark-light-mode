fbb27a95a730e9b45b8e07c61ec76927a48b415c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-02 17:45:38 +1200

    styled the container

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { MdLightMode, MdDarkMode } from 'react-icons/md'
 import Button from './Button'
 
-
-const Header = ({toggleTheme}) => {
+const Header = ({ toggleTheme }) => {
 	return (
-		<div className='container'>
+		<>
 			<header>
-				<h1>The Fuse</h1>
-				<Button MdLightMode={MdLightMode} MdDarkMode={MdDarkMode} toggleTheme ={toggleTheme}/>
+				<div className='container'>
+					<h1>The Fuse</h1>
+					<Button
+						MdLightMode={MdLightMode}
+						MdDarkMode={MdDarkMode}
+						toggleTheme={toggleTheme}
+					/>
+				</div>
 			</header>
-		</div>
+		</>
 	)
 }
 
