9b502419d8267c168e75b26e7cde770349d40e5e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-08 05:50:53 +1200

    added context.jd to avoid prop drilling

---
+++ src/context.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const getLocalStorageTheme = () => {
+	let theme = 'light-theme'
+	if (localStorage.getItem('theme')) {
+		theme = localStorage.getItem('theme')
+	}
+	return theme
+}
+
+const AppProvider = ({ children }) => {
+	const [theme, setTheme] = useState(getLocalStorageTheme())
+	const [active, setActive] = useState(false)
+
+	const toggleTheme = () => {
+			if (theme === 'light-theme') {
+			setTheme('dark-theme')
+			setActive(true)
+		} else {
+			setTheme('light-theme')
+			setActive(false)
+		}
+	}
+
+	return (
+		<AppContext.Provider value={{ theme, active, toggleTheme }}>
+			{children}
+		</AppContext.Provider>
+	)
+}
+
+//custom hook
+
+export const useGlobalContext = () => {
+	return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
